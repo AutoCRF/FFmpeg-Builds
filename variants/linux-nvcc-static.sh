643c24524e85011729245b42e78f647cc467022a
Lawrence Curtis <lawrencecurtis@yellowpad.co.uk>
2025-12-28 17:02:49 +0000

    NVCC

---
+++ variants/linux-nvcc-static.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug --enable-cuda-nvcc --enable-nonfree --disable-ffplay "
+FF_CFLAGS=""
+FF_CXXFLAGS=""
+FF_LDFLAGS=""
+GIT_BRANCH="master"
+LICENSE_FILE=""
+
+package_variant() {
+    IN="$1"
+    OUT="$2"
+
+    mkdir -p "$OUT"
+    cp "$IN"/bin/* "$OUT"
+}
